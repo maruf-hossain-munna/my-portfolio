0803e16859b112aebb21e6b851a7733e4ae4cd26
maruf21hossain <marufhossainmunna21@gmail.com>
2022-12-09 20:46:31 +0600

    Home page done

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import TopHomeBanner from '../TopHomeBanner/TopHomeBanner';
-import DownloadResume from './DownloadResume';
 
 const Home = () => {
     return (
         <div>
             <TopHomeBanner></TopHomeBanner>
-            <h2>This is home page</h2>
-            <DownloadResume></DownloadResume>
         </div>
     );
 };
